989f2eeca830b3c3a2d63f5327faa6ebddb201e8
Kavi Chameleon <kavichameleon@gmail.com>
2025-02-28 12:10:55 +1030

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub mod ai_sharding;
+pub mod fraud_detection;
+pub mod predictive_analytics;
+pub mod model_loader;
+pub mod ai_consensus;
+pub mod ai_optimization;
+
+pub use ai_sharding::*;
+pub use fraud_detection::*;
+pub use predictive_analytics::*;
+pub use model_loader::*;
+pub use ai_consensus::*;
+pub use ai_optimization::*;
